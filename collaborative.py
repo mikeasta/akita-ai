4862969e87447f9db9ef9ffea28fce99f108ffa8
Mikhail Astashonak <thenaitrise@gmail.com>
2021-05-20 01:03:57 +0300

    Update collaborative.py

--- collaborative.py
+++ collaborative.py
@@ -4,22 +4,23 @@
 import numpy as np
 import json_implementation as ji
 
+
 # Getting data
 ratings = ji.json_get_data()["collaborative_filtering_ratings"]
+
 
 # New user stats
 handling_user_ratings = [1, 3, 0, 0, 0]
 
+
 # Cosine calculation
 def cosine(ratings_a, ratings_b):
-    vector_a = np.array(ratings_a)
-    vector_b = np.array(ratings_b)
+    length_a = np.linalg.norm(ratings_a)
+    length_b = np.linalg.norm(ratings_b)
 
-    length_a = np.linalg.norm(vector_a)
-    length_b = np.linalg.norm(vector_b)
-
-    distance = np.dot(vector_a, vector_b) / np.dot(length_a, length_b)
+    distance = np.dot(ratings_a, ratings_b) / np.dot(length_a, length_b)
     return distance
+
 
 # Result array
 cosine_values = []
@@ -27,7 +28,8 @@ for user_ratings in ratings:
     current_distance = cosine(handling_user_ratings, user_ratings)
     cosine_values.append({current_distance: user_ratings})
 
+    
 # Descending sort
 cosine_values.sort(key=lambda item: sorted(list(item.keys())), reverse=True)
- 
+
 print(cosine_values)
